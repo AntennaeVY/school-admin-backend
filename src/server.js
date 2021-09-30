465976936dc55374c917add94977a043378bd662
HyperNovaVY <hypernovavy1995@gmail.com>
2021-09-30 14:15:57 -0300

    Cors issue

--- src/server.js
+++ src/server.js
@@ -20,7 +20,7 @@ app.set("PORT", process.env.PORT || 3000);
 require("./database/database");
 
 // Middlewares
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api", routes);
